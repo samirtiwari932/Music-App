79a2225c05eef7990983ca5f670fea9422857763
Sameer <samirtiwari567@gmail.com>
2024-06-01 20:36:07 +0545

    multi column view ui for recommended audios

--- PodifyPlayer/src/components/RecommendedAudios.tsx
+++ PodifyPlayer/src/components/RecommendedAudios.tsx
@@ -1,29 +1,32 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
 import React from 'react'
 import { useFetchRecommendedtAudios } from 'src/hooks/query'
 import colors from 'src/utilis/color'
+import GridView from '@ui/GridView'
 
 interface Props { }
 const RecommendedAudios = (props: Props) => {
 
     const { data, isLoading } = useFetchRecommendedtAudios()
-    console.log(data)
+
+
+
+    const getPoster = (poster?: string) => {
+        return poster ? { uri: poster } : require('../assets/music.jpg')
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>
                 Latest uploads
             </Text>
-            <View style={{ width: '100%', flexDirection: 'row', flexWrap: 'wrap' }}>
-                {data?.map((item) => {
+            <GridView
+                col={3}
+                data={data || []} renderItem={(item) => {
                     return (
-                        <View style={{ width: '33.33%' }} key={item.id}>
-                            <View style={{
-                                padding: 5
-                            }}>
-                                <Image source={{ uri: item.poster }} style={{ width: '100%', aspectRatio: 1, borderRadius: 7 }} />
-                            </View>
+                        <Pressable>
+                            <Image source={getPoster(item.poster)} style={styles.poster} />
                             <Text
-
                                 numberOfLines={2}
                                 ellipsizeMode='tail'
                                 style={{
@@ -32,12 +35,12 @@ const RecommendedAudios = (props: Props) => {
                                     fontSize: 15,
                                     marginTop: 5
                                 }}>
-                                {item.title}
+                                {item.title}p
                             </Text>
-                        </View>
+                        </Pressable>
+
                     )
-                })}
-            </View>
+                }} />
         </View>
     )
 }
@@ -51,6 +54,7 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         marginBottom: 15
     },
+    poster: { width: '100%', aspectRatio: 1, borderRadius: 7 }
 })
 
 export default RecommendedAudios 
