145a07c92f4629f22e4ad21ba00308f2d90fe7cc
Sameer <samirtiwari567@gmail.com>
2024-06-02 22:33:30 +0545

    playlistModal ui setup

---
+++ PodifyPlayer/src/components/PlayListModal.tsx
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet, Pressable, ScrollView } from 'react-native'
+import React, { ReactNode } from 'react'
+import BasicModalContainer from '@ui/BasicModalContainer'
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
+import colors from 'src/utilis/color'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import { Playlist } from 'src/@types/audio'
+
+interface Props {
+    visible: boolean,
+    onRequestClose?(): void,
+    list: Playlist[]
+}
+
+interface ListItemProps {
+    title: string,
+    icon: ReactNode,
+
+}
+
+const ListItem = ({ title, icon }: ListItemProps) => {
+    return (
+        <Pressable style={styles.listItemContainer}>
+            {icon}
+            <Text style={styles.listItemTitle}>{title} </Text>
+        </Pressable>
+    )
+}
+const PlayListModal = ({ onRequestClose, visible, list }: Props) => {
+    return (
+        <BasicModalContainer visible={visible} onRequestClose={onRequestClose} >
+            {/* we want to render playlist here */}
+            <ScrollView>
+                {list.map((item) => {
+                    return <ListItem key={item.id} title={item.title} icon={<FontAwesomeIcon size={20} name={item.visibility === "public" ? "globe" : "lock"} color={colors.PRIMARY} />} />
+                })}
+
+            </ScrollView>
+
+            {/* create playlist new button  */}
+            <ListItem icon={<AntDesign size={20} name='plus' color={colors.PRIMARY} />} title='Create new' />
+
+        </BasicModalContainer >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {},
+    listItemContainer: {
+        flexDirection: "row",
+        alignItems: 'center',
+        height: 45
+    },
+    listItemTitle: {
+        fontSize: 16,
+        color: colors.PRIMARY
+    }
+})
+
+export default PlayListModal 
--- PodifyPlayer/src/views/Home.tsx
+++ PodifyPlayer/src/views/Home.tsx
@@ -1,67 +1,114 @@
-import LatestUpload from '@components/LatestUpload'
-import OptionsModal from '@components/OptionsModal'
-import RecommendedAudios from '@components/RecommendedAudios'
-import React, { useState } from 'react'
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import MaterialComIcons from "react-native-vector-icons/MaterialCommunityIcons"
-import colors from 'src/utilis/color'
-
+import LatestUpload from '@components/LatestUpload';
+import OptionsModal from '@components/OptionsModal';
+import PlayListModal from '@components/PlayListModal';
+import RecommendedAudios from '@components/RecommendedAudios';
+import { FC, useState } from 'react';
+import { View, StyleSheet, Pressable, Text } from 'react-native';
+import MaterialComIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useDispatch } from 'react-redux';
+import { AudioData } from 'src/@types/audio';
+import catchAsyncError from 'src/api/catchError';
+import client from 'src/api/client';
+import { updateNotification } from 'src/store/notification';
+import { Keys, getFromAsyncStorage } from 'src/utilis/asyncStorage';
+import colors from 'src/utilis/color';
 
 interface Props { }
-const Home = (props: Props) => {
-    const [showOptions, setShowOptions] = useState(false)
+
+const Home: FC<Props> = props => {
+    const [showOptions, setShowOptions] = useState(false);
+    const [selectedAudio, setSelectedAudio] = useState<AudioData>();
+
+    const dispatch = useDispatch();
+
+    const handleOnFavPress = async () => {
+        if (!selectedAudio) return;
+        // send request with the audio id that we want to add to fav
+
+        try {
+            const token = await getFromAsyncStorage(Keys.Auth_Token);
+
+            const { data } = await client.post(
+                `/favorite?audioId=${selectedAudio.id}`,
+                null,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                },
+            );
+            console.log(data, "This is the data")
+        } catch (error) {
+            const errorMessage = catchAsyncError(error);
+            dispatch(updateNotification({ message: errorMessage, type: 'error' }));
+        }
+
+        setSelectedAudio(undefined);
+        setShowOptions(false);
+    };
+
+    const handleOnLongPress = (audio: AudioData) => {
+        setSelectedAudio(audio);
+        setShowOptions(true);
+    };
+
     return (
         <View style={styles.container}>
-            <LatestUpload onAudioPress={(item) => {
-                console.log(item)
-            }}
-                onAudioLongPress={() => {
-                    setShowOptions(true)
-                }} />
-            <RecommendedAudios onAudioPress={(item) => {
-                console.log('audio press', item)
-            }}
-                onAudioLongPress={() => {
-                    setShowOptions(true)
-                }} />
+            <LatestUpload
+                onAudioPress={item => {
+                    console.log(item);
+                }}
+                onAudioLongPress={handleOnLongPress}
+            />
+            <RecommendedAudios
+                onAudioPress={item => {
+                    console.log(item);
+                }}
+                onAudioLongPress={handleOnLongPress}
+            />
             <OptionsModal
                 visible={showOptions}
                 onRequestClose={() => {
-                    setShowOptions(false)
+                    setShowOptions(false);
                 }}
-
-                options={
-                    [{ title: "Add to playlist", icon: "playlist-music" }, { title: "Add to Fav List", icon: 'cards-heart' }]
-                }
+                options={[
+                    { title: 'Add to playlist', icon: 'playlist-music' },
+                    {
+                        title: 'Add to favorite',
+                        icon: 'cards-heart',
+                        onPress: handleOnFavPress,
+                    },
+                ]}
                 renderItem={item => {
                     return (
-                        <Pressable
-                            style={styles.optionsContainer}>
-                            <MaterialComIcons size={24} name={item.icon} color={colors.PRIMARY} />
-                            <Text style={styles.optionLabel} >
-                                {item.title}
-                            </Text>
+                        <Pressable onPress={item.onPress} style={styles.optionContainer}>
+                            <MaterialComIcon
+                                size={24}
+                                color={colors.PRIMARY}
+                                name={item.icon}
+                            />
+                            <Text style={styles.optionLabel}>{item.title}</Text>
                         </Pressable>
-                    )
-                }} />
+                    );
+                }}
+            />
+            <PlayListModal visible list={[{ title: "PlayList one", visibility: 'private', id: "1" },
+            { title: "PlayList two", visibility: 'public', id: "2" }
+            ]} />
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         padding: 10,
     },
-    optionsContainer: {
-        flexDirection: "row",
-        alignItems: "center",
+    optionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 10,
     },
-    optionLabel: {
-        marginLeft: 5,
-        color: colors.PRIMARY,
-        fontSize: 18,
-    }
+    optionLabel: { color: colors.PRIMARY, fontSize: 16, marginLeft: 5 },
+});
 
-})
-
-export default Home 
+export default Home;
